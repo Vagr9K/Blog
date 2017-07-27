9220ae379b5e592d3ca98c0a1bf83fd3f04608da
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-27 05:25:29 +0400

    Updates avatar.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -14,7 +14,7 @@ module.exports = {
   userName: "Ruben Harutyunyan", // Username to display in the author segment.
   userTwitter: "vagr9k", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "", // User location to display in the author segment.
-  userAvatar: "/logos/logo-128.png", // User avatar to display in the author segment.
+  userAvatar: "/logos/avatar.png", // User avatar to display in the author segment.
   userDescription:
     "I'm an enthusiastic software developer from Armenia. I am using modern technologies like ReactJS and NodeJS to create next generation applications.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
