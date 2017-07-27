2adc99b19ba60dceb12c1f0b7f23762ce773323a
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-27 10:42:17 +0400

    Updates siteURL.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   siteTitle: "Ruben's Blog", // Site title.
   siteTitleAlt: "Vagr9K's Blog", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
+  siteUrl: "https://vagr9k.me", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   siteDescription: "My personal blog where I write about life and technology.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
