9009b9c7e7e417f9893f6f87dd745b03824655a9
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-27 19:08:17 +0400

    ToolbarTitle imrovements.

--- src/components/ToolbarTitle/ToolbarTitle.jsx
+++ src/components/ToolbarTitle/ToolbarTitle.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
-import Avatar from "react-md/lib/Avatars";
-import config from "../../../data/SiteConfig";
-import "./ToolbarTitle.scss";
+import React, { Component } from 'react';
+import Avatar from 'react-md/lib/Avatars';
+import Link from 'gatsby-link';
+import config from '../../../data/SiteConfig';
+import './ToolbarTitle.scss';
 
 export default class ToolbarTitle extends Component {
   render() {
     const { userAvatar } = config;
     return (
-      <div className="toolbar-title">
-        <Avatar src={userAvatar} role="presentation" />
+      <Link to="/" className="toolbar-title">
+        <Avatar src={userAvatar} role="presentation" className="toolbar-avatar" />
         <h2 className="toolbar-title-text">
           {config.siteTitle}
         </h2>
-      </div>
+      </Link>
     );
   }
 }
